28caf8eec3400bf97866b16f4c29be7a3d82f497
Emre Deniz <emre.deniz@outlook.com>
2024-01-27 11:13:39 -0500

    Datastore class for managing user data

--- app/src/main/java/com/example/mapd721_a1_emredeniz/datastore/StoreUserInfo.kt
+++ app/src/main/java/com/example/mapd721_a1_emredeniz/datastore/StoreUserInfo.kt
@@ -14,5 +14,46 @@ import kotlinx.coroutines.flow.map
 
 class StoreUserInfo(private val context: Context) {
 
+    companion object {
+        private val Context.dataStore: DataStore<Preferences> by preferencesDataStore("UserInfo")
 
+        // Keys to identify user name, email and id in DataStore
+        val USER_NAME_KEY = stringPreferencesKey("user_name")
+        val USER_EMAIL_KEY = stringPreferencesKey("user_email")
+        val USER_ID_KEY = stringPreferencesKey("user_id")
+    }
+
+    // Flow: the user's stored name
+    val getName: Flow<String?> = context.dataStore.data
+        .map { preferences ->
+            preferences[USER_NAME_KEY] ?: ""
+        }
+
+    // Flow: the user's stored email
+    val getEmail: Flow<String?> = context.dataStore.data
+        .map { preferences ->
+            preferences[USER_EMAIL_KEY] ?: ""
+        }
+
+    // Flow: the user's stored id
+    val getId: Flow<String?> = context.dataStore.data
+        .map { preferences ->
+            preferences[USER_ID_KEY] ?: ""
+        }
+
+    // Save user name, email and id in DataStore
+    suspend fun saveInfo(name: String, email: String, id: String) {
+        context.dataStore.edit { preferences ->
+            preferences[USER_NAME_KEY] = name
+            preferences[USER_EMAIL_KEY] = email
+            preferences[USER_ID_KEY] = id
+        }
+    }
+
+    // Delete user name, email and id in DataStore
+    suspend fun clearInfo() {
+        context.dataStore.edit { preferences ->
+            preferences.clear()
+        }
+    }
 }
